24f365aadbf457d77e7c6e5770f2628edae78b1b
Frozzy2021 <87904196+Frozzy2021@users.noreply.github.com>
2022-02-07 19:36:01 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+    db: 'mongodb+srv://frozzy:89374375985@cluster30.wtofz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', /* MongoDB connection link */
+    secretKey: '6XfZGRiKvYfW4tOr5fgr', /* VK Mini Apps service secret key */
+    user_id: 474370813, /* VK user id */
+    token: 'a3c5526c50ac0322907dbddb30c7b2be69eff9caf239caa366c60e26ce71037253de38b07269e61fb9118', /* VK access token */
+    key: '81UP2u2bD07e=C__&2ap-HZ;9B0mVhDnXGsccw]K29G_xmTgbp' // VK Coin merchant key
+
+};
